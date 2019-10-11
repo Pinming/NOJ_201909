178d4d3d95a9e46b94f678a0d66ea55a43de78ce
pinming <zhengtingpeng88@163.com>
2019-10-11 21:38:17 +0800

    191011-2138

---
+++ t023.cpp
@@ -0,0 +1,24 @@
+# include <iostream>
+# include <cmath>
+using namespace std;
+int main()
+{
+	int N, i = 7,a;
+	cin >> N;
+	for (i = 1; i <= N; i++)
+	{
+		if (i % 7 == 0)
+			cout << i << " ";
+		else
+		{
+			a = i;
+			while (a > 0)
+			{
+				if (a % 10 == 7)
+					cout << i << ' ';
+				a = a / 10;
+			}
+		}
+	}
+	return 0;
+}
